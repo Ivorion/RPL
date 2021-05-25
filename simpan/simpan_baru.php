24add53b69a9d09d5d30f55d2cb90c48dc5e9515
Ivorion <ivanhasibuan827@gmail.com>
2021-05-24 23:03:46 -0700

    membuat folder simpan

---
+++ simpan/simpan_baru.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="simpanan.css">
+    <title>Buat Simpanan Baru</title>
+</head>
+<body>
+    <h1>Buat Simpanan Baru</h1>
+    <hr>
+    <form action="">
+        <table>
+            <tr>
+                <td>Nama: </td>
+                <td><input type="text" name='nama'></td>
+            </tr>
+            <tr>
+                <td>No Simpanan: </td>
+                <td><input type="text" name='noSimpanan'></td>
+            </tr>
+            <tr>
+                <td>Tgl Simpanan: </td>
+                <td><input type="text" name='tglSimpanan'></td>
+            </tr>
+            <tr>
+                <td>Besar Simpanan: </td>
+                <td><input type="text" name='besarPinjaman'></td>
+            </tr>
+        </table>
+        <hr>
+        <button name='ajukanSimpanan'>Ajukan Simpanan</button>
+    </form>
+</body>
+</html>
