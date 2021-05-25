4953f365d054711f195b4cdb67cdba363ca290f0
Ivorion <ivanhasibuan827@gmail.com>
2021-05-24 20:38:46 -0700

    modifikasi file index.php

--- landingPage/index.php
+++ landingPage/index.php
@@ -5,10 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="normalize.css">
-    <link rel="stylesheet" type="text/css" href="landingPage.css">
+    <link rel="stylesheet" type="text/css" href="dashboard.css">
     <title>Aplikasi Simpan Pinjam</title>
 </head>
 <body>
-    <img class="bg" src="background.jpg" alt="">
+
+    <div class="container">
+        <h1 class='judul'>Welcome, Ivan!</h1>
+        <hr>
+        <img src="profileIcon.png" alt=""><span class='profileName'>Ivan</span>
+        <hr>
+        <p class='prompt'>Apa yang ingin anda lakukan hari ini?</p>
+        <table>
+            <tr>
+                <td><button class='buttonSimpan'>Simpan</button></td>   
+                <td><button class='buttonPinjam'>Pinjam</button></td>
+            </tr>
+        </table>
+    </div>
+
 </body>
 </html>
