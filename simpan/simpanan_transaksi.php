29cf043d23f8c107a229cc2047cca72cb5b0bd2b
Ivorion <ivanhasibuan827@gmail.com>
2021-05-30 19:08:54 -0700

    menambahkan file simpanan_transaksi

---
+++ simpan/simpanan_transaksi.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="simpanan.css">
+    <title>Transaksi Simpanan</title>
+</head>
+<body>
+    <h1>Transaksi Simpanan</h1>
+    <hr>
+    <form action="">
+        <table>
+            <tr>
+                <td>Nama: </td>
+                <td><input type="text" name='nama'></td>
+            </tr>
+            <tr>
+                <td>No Simpanan: </td>
+                <td><input type="text" name='noSimpanan'></td>
+            </tr>
+            <tr>
+                <td>Tgl Simpanan: </td>
+                <td><input type="text" name='tglSimpanan'></td>
+            </tr>
+            <tr>
+                <td>Besar Simpanan: </td>
+                <td><input type="text" name='besarSimpanan'></td>
+            </tr>
+            <tr>
+                <td>No. Rekening</td>
+                <td><input type="text" name='noRekening'></td>
+            </tr>
+        </table>
+        <hr>
+        <button name='kirim'>Kirim</button>
+    </form>
+</body>
+</html>
